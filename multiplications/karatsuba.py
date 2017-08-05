99d070bb5dfcb00ee32171041c76f91806df3ac9
Jovan <jtomasevic@gmail.com>
2017-08-05 03:02:22 +0200

    still seting up project add some example implementation

--- multiplications/karatsuba.py
+++ multiplications/karatsuba.py
@@ -1,5 +1,20 @@
 """ Karatsuba algorithm for multiplication """
 
-
-def multipy(a, b):
-    print "test"
+def multiply(x,y):
+    """ Karatsuba algorithm for multiplication """
+    if len(str(x)) == 1 or len(str(y)) == 1:
+        # last step in recursion, now go back
+        return x * y
+    else:
+        n = max(len(str(x)),len(str(y)))
+        nby2 = n / 2
+        a = x / 10**(nby2)
+        b = x % 10**(nby2)
+        c = y / 10**(nby2)
+        d = y % 10**(nby2)
+        ac = multiply(a,c)
+        bd = multiply(b,d)
+        ad_plus_bc = multiply(a+b,c+d) - ac - bd
+        # this little trick, writing n as 2*nby2 takes care of both even and odd n
+        prod = ac * 10**(2*nby2) + (ad_plus_bc * 10**nby2) + bd
+        return prod
--- test.py
+++ test.py
@@ -1,4 +1,14 @@
 from multiplications import karatsuba  
 
-karatsuba.multipy(4,5)
+print karatsuba.multiply(4,5)
+print 4*5
+print karatsuba.multiply(10,50)
+print 10*50
+print karatsuba.multiply(21,21)
+print 21*21
+print karatsuba.multiply(64,789)
+print 64*789
+print karatsuba.multiply(301,5)
+print 301*5
+
 
